1dc75fd74153fb69e9e7dc79d1a3192ed471165f
Lucien KOHLI <lucien.kohli@gmail.com>
2023-11-29 22:55:51 +0100

    UdapteJS&CSSroom3

--- 003-ROOM3.js
+++ 003-ROOM3.js
@@ -4,3 +4,24 @@ scrollContainer.addEventListener("wheel", (evt) => {
   evt.preventDefault();
   scrollContainer.scrollLeft += evt.deltaY;
 });
+
+const newContainer = document.querySelector(".container");
+const newGallery = document.querySelector(".gallery");
+
+const cloned = newGallery.cloneNode(true);
+newContainer.appendChild(cloned);
+cloned.style.right = newGallery.offsetWidth + "px";
+
+newContainer.style.display = "flex";
+
+const threshold = 120;
+
+window.addEventListener("scroll", () => {
+  const halfWidth = newGallery.clientWidth;
+
+  if (window.scrollX > halfWidth) {
+    window.scrollTo(0, window.scrollX - halfWidth);
+  } else if (window.scrollX < threshold) {
+    window.scrollTo(0, halfWidth + window.scrollX);
+  }
+});
