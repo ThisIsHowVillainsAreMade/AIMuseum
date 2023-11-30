59eab741dbea136209a035dc6fb2d89304d67d9b
Hamidou Mamadou <midou88530@gmail.com>
2023-11-30 17:00:17 +0100

    modify HTML/CSS to place images randomly

--- room2.js
+++ room2.js
@@ -1,3 +1,21 @@
 function move(e) {
-      window.scrollTo(e.clientX - window.innerWidth /20, 2*e.clientY - window.innerHeight /20);
+      window.scrollTo(1.5*e.clientX - window.innerWidth / 1.1, 2.5*e.clientY - window.innerHeight / 1.1);
+      
 }
+/*const container = document.getElementById('container');
+let delay = 100;
+let mouseX = 0;
+let mouseY = 0;
+
+function move(e) {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+  setTimeout(updateTransform, delay);
+}
+
+function updateTransform() {
+  const deltaX = (1.5 * mouseX - window.innerWidth / 1.01);
+  const deltaY = (1.7 * mouseY - window.innerHeight / 1.01);
+  container.style.transition = 'transform 0.3s ease-out'; // Ajustez la durée et le timing function selon vos besoins
+  container.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
+}*/
