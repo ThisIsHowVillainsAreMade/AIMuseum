75b017592a6087cffb21434eabe12f09b32e4ca3
ThisIsHowVillainsAreMade <okalfa@outlook.fr>
2023-12-02 21:01:49 +0100

    creation of the final sheet

---
+++ about/parallax.js
@@ -0,0 +1,16 @@
+let robots = document.getElementById("robots");
+let moutain = document.getElementById("mountain");
+let sky = document.getElementById("sky");
+let moon = document.getElementById("moon");
+let aivolution = document.getElementById("aivolution");
+
+window.addEventListener("scroll", () => {
+  let { scrollY } = window;
+
+  (robots.style.top = 100), 5 * scrollY + "px";
+  moutain.style.top = 150 + 0.05 * scrollY + "px";
+  moon.style.left = 1.1 * scrollY + "px";
+  sky.style.top = -1.2 * scrollY + "px";
+  aivolution.style.top = 300 + -3 * scrollY + "px";
+  // aivolution.style.top = 120 + -1.5 * scrollY + "px";
+});
