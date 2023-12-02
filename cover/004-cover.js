9eddcbcf300e14ebfb2cb8ab723873c161d73668
ThisIsHowVillainsAreMade <okalfa@outlook.fr>
2023-12-02 13:09:46 +0100

    modifications on parallax

--- cover/004-cover.js
+++ cover/004-cover.js
@@ -2,14 +2,15 @@ let road = document.getElementById("road");
 let moutain = document.getElementById("mountain");
 let sky = document.getElementById("sky");
 let moon = document.getElementById("moon");
-let midnight = document.getElementById("midnight");
+let aivolution = document.getElementById("aivolution");
 
 window.addEventListener("scroll", () => {
   let { scrollY } = window;
 
-  (road.style.top = 0), 5 * scrollY + "px";
+  (road.style.top = 100), 5 * scrollY + "px";
   moutain.style.top = 150 + 0.05 * scrollY + "px";
   moon.style.left = 1.1 * scrollY + "px";
   sky.style.top = -1.2 * scrollY + "px";
-  midnight.style.top = 120 + -1.5 * scrollY + "px";
+  aivolution.style.top = 300 + -3 * scrollY + "px";
+  // aivolution.style.top = 120 + -1.5 * scrollY + "px";
 });
