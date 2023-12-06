bd585174a18fdb33b04f8ba91a3f2edf22bc1ea4
Océane K <146114113+ThisIsHowVillainsAreMade@users.noreply.github.com>
2023-12-06 12:44:44 +0100

    some modifications and optimisation of carousel navigation

--- about/parallax.js
+++ about/parallax.js
@@ -1,17 +1,38 @@
-let robots = document.getElementById("robots");
-let moutain = document.getElementById("mountain");
-let sky = document.getElementById("sky");
-let moon = document.getElementById("moon");
-let aivolution = document.getElementById("aivolution");
+// COVER PARALLAX CONTENT.
 
-let modalAbout = document.getElementById("about-page");
+const robots = document.getElementById("robots");
+const moutain = document.getElementById("mountain");
+const sky = document.getElementById("sky");
+const moon = document.getElementById("moon");
+const aivolution = document.getElementById("aivolution");
+
+const modalAbout = document.getElementById("about-page");
+
+// BACKGROUND TITLE PARALLAX CONTENT.
+
+// littleGirlAiPicture
+// AiBotPicture
+// faceToFaceAiPicture
+// tomorrowPicture
+
+// function parallaxPictures() {
+//   const modalScroll = -modalAbout.scrollTop;
+//   const contentPictures = document.querySelectorAll(".backgroundPictures");
+//   ContentPictures.forEach(function (image) {
+//     const speed = image.getAttribute("data-speed");
+//     const yPos = scrollTop * speed;
+//     image.style.transform = "translateY(" + yPos + "px)";
+//   });
+// }
+
+// MODAL SCROLL
 
 modalAbout.addEventListener("scroll", () => {
-  let { scrollTop } = modalAbout;
-
+  const { scrollTop } = modalAbout;
   robots.style.top = -200 + 0.1 * scrollTop + "px";
   moutain.style.top = 150 + 0.05 * scrollTop + "px";
   moon.style.left = 1.1 * scrollTop + "px";
   sky.style.top = -1.2 * scrollTop + "px";
-  aivolution.style.top = 500 + -5 * scrollTop + "px";
+  aivolution.style.top = 500 + -1 * scrollTop + "px";
+  // parallaxPictures();
 });
